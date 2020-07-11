c88a09d7e6bb350e605e6678711aaf6238e2c886
Gáspár József Dániel <gaspar.jozsef.daniel@gmail.com>
2020-07-11 16:30:31 +0200

    Fix light particle movement and add mirrorplacer

--- src/gmtk-jam-2020/Assets/scripts/light/LightMovement.cs
+++ src/gmtk-jam-2020/Assets/scripts/light/LightMovement.cs
@@ -8,7 +8,7 @@ public class LightMovement : MonoBehaviour
     [SerializeField] private float _speed;
     [SerializeField] private float _radius;
     [SerializeField] private Vector2 _direction;
-    [SerializeField] private float _speedIncrease = 1.1f;
+    [SerializeField] private float _speedIncrease = 0.5f;
 
     public Vector3 Direction
     {
@@ -20,14 +20,11 @@ public class LightMovement : MonoBehaviour
     {
 
         var hit = Physics2D.Raycast(this.transform.position, _direction, _speed * Time.deltaTime);
-        if (hit.collider != null && hit.distance < _radius)
-        {
-            _direction = Vector2.Reflect(_direction, hit.normal).normalized;
-            _speed *= _speedIncrease;
-        }
-        else if(hit.collider != null)
+        if (hit.collider != null)
         {
             this.transform.position = hit.point + (-_direction * _radius);
+            _direction = Vector2.Reflect(_direction, hit.normal).normalized;
+            _speed += _speedIncrease;
         }
         else
         {
---
+++ src/gmtk-jam-2020/Assets/scripts/systems/MirrorPlacer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA;
+
+public class MirrorPlacer : MonoBehaviour
+{
+    [SerializeField] private GameObject _mirror;
+
+    private Plane plane;
+    private Vector3? startPos;
+    private GameObject obj;
+
+    private void Start()
+    {
+        plane = new Plane(-Vector3.forward, Vector3.zero);
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (plane.Raycast(ray, out float distance))
+            {
+                startPos = ray.GetPoint(distance);
+                obj = GameObject.Instantiate(_mirror);
+                obj.transform.position = startPos.Value;
+            }
+        }
+        if (startPos.HasValue)
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (plane.Raycast(ray, out float distance))
+            {
+                var endPos = ray.GetPoint(distance);
+                obj.transform.up = (endPos - startPos.Value).normalized;
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                startPos = null;
+                obj = null;
+            }
+        }
+
+    }
+}
