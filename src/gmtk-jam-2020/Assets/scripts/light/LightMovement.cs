079e48da302c25c3bf3d4f850e775b3daaa07672
Gáspár József Dániel <gaspar.jozsef.daniel@gmail.com>
2020-07-11 15:08:09 +0200

    Add base game

---
+++ src/gmtk-jam-2020/Assets/scripts/light/LightMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private Vector3 _direction;
+
+    void Update()
+    {
+        this.transform.position += _direction * _speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("AAAAAAAAAAA");
+        var tr = collision.transform.parent.transform;
+        _direction = Vector3.Reflect(_direction, tr.up).normalized;
+    }
+}
